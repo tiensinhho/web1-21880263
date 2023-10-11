93a8c840d3929aabe82227889ec140eb5b84e496
Sinh Ho <sinh.ho@hotmail.com>
2023-10-11 17:10:12 +0700

    update

--- verify.php
+++ verify.php
@@ -2,7 +2,7 @@
 $_POST = json_decode(file_get_contents('php://input'), true);
 
 if (isset($_POST) && isset($_POST['g-token'])) {
-    $secretKey = '6LcDy5EoAAAAAPp0PWh1w2NufCo_MUMlJvIs_SVa';
+    $secretKey = '6LcE6I8oAAAAAJ_haAWga4Zi0xL3r9TY2vwiVfhm';
     $token = $_POST['g-token'];
     $ip = $_SERVER['REMOTE_ADDR'];
 
