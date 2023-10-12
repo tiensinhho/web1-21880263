492e7fa97d9005962f0562b1aacd67cce245f5a1
Sinh Ho <sinh.ho@hotmail.com>
2023-10-12 13:44:15 +0700

    update

--- verify.php
+++ verify.php
@@ -1,22 +1,24 @@
 <?php
-$_POST = json_decode(file_get_contents('php://input'), true);
 
-if (isset($_POST) && isset($_POST['g-token'])) {
-    $secretKey = '6LeFmZIoAAAAAIKjM1uzEuxL-i6piOqe8nryvNFk';
-    $token = $_POST['g-token'];
-    $ip = $_SERVER['REMOTE_ADDR'];
+if($_SERVER['REQUEST_METHOD']=="POST"){
+    $_POST = json_decode(file_get_contents('php://input'), true);
+    if (isset($_POST) && isset($_POST['g-token'])) {
+        $secretKey = '6LeFmZIoAAAAAIKjM1uzEuxL-i6piOqe8nryvNFk';
+        $token = $_POST['g-token'];
+        $ip = $_SERVER['REMOTE_ADDR'];
 
-    $url = "https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$token."&remoteip=".$ip;
-    $request = file_get_contents($url);
-    $response = json_decode($request); 
+        $url = "https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$token."&remoteip=".$ip;
+        $request = file_get_contents($url);
+        $response = json_decode($request); 
 
-    if ($response->success && $response->action == 'submit' && $response->score >= 0.5) {
-        header('HTTP/1.1 200 OK');
-    } else {
-        header('HTTP/1.1 401 Unauthorized');
-    }
-    
-    echo json_encode($response);
-    exit();
-} 
+        if ($response->success && $response->action == 'submit' && $response->score >= 0.5) {
+            header('HTTP/1.1 200 OK');
+        } else {
+            header('HTTP/1.1 401 Unauthorized');
+        }
+        
+        echo json_encode($response);
+        exit();
+    } 
+}
 ?>
