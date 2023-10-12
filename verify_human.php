aa3ca6c530c17ec11123b7c0aa05c70f641548d0
Sinh Ho <sinh.ho@hotmail.com>
2023-10-12 13:00:06 +0700

    update

---
+++ verify_human.php
@@ -0,0 +1,35 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD']=="POST"){
+
+    if(isset = $_POST['g-recaptcha-response'];
+        $token = $_POST['g-recaptch-response'];
+        $url = 'https://www.google.com/recaptcha/api/siteverify';
+        $data = array(
+            'secret' =>'6LeFmZIoAAAAAIKjM1uzEuxL-i6piOqe8nryvNFk',
+            'response' => $token
+        );
+
+        $options = array(
+            'http' => array(
+                'header' => "Content-Type: application/x-www-form-urlendcoded\r\n",
+                'method' => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+
+        $context = stream_context_create($options);
+        $result = file_get_contents($url,false, $context );
+        $response = json_decode($result);
+
+        if ($response->success && $response->action == 'submit' && $response->score >= 0.5) {
+            header('HTTP/1.1 200 OK');
+        } else {
+            header('HTTP/1.1 401 Unauthorized');
+        }
+        
+        echo json_encode($response);
+        exit();
+        
+    )
+}
