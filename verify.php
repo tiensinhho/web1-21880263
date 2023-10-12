980c5513a8897b9d9c6dfe2a7d2aa6768374ab5b
Sinh Ho <sinh.ho@hotmail.com>
2023-10-12 13:51:55 +0700

    update

--- verify.php
+++ verify.php
@@ -1,13 +1,13 @@
 <?php
 
 if($_SERVER['REQUEST_METHOD']=="POST"){
-    $_POST = json_decode(file_get_contents('php://input'), true);
-    if (isset($_POST) && isset($_POST['g-token'])) {
+    // $_POST = json_decode(file_get_contents('php://input'), true);
+    if (isset($_POST) && isset($_POST['g-recaptcha-response'])) {
         $secretKey = '6LeFmZIoAAAAAIKjM1uzEuxL-i6piOqe8nryvNFk';
-        $token = $_POST['g-token'];
+        $token = $_POST['g-recaptcha-response'];
         $ip = $_SERVER['REMOTE_ADDR'];
 
-        $url = "https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$token."&remoteip=".$ip;
+        $url = "https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$token;
         $request = file_get_contents($url);
         $response = json_decode($request); 
 
