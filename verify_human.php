a12ace8d532da204e22074288d97ae00120016e5
Sinh Ho <sinh.ho@hotmail.com>
2023-10-12 13:30:35 +0700

    update

--- verify_human.php
+++ verify_human.php
@@ -22,7 +22,7 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
         $result = file_get_contents($url,false, $context );
         $response = json_decode($result);
 
-        if ($response->success && $response->action == 'submit' && $response->score >= 0.5) {
+        if ($response->success && $response->score >= 0.5) {
             header('HTTP/1.1 200 OK');
         } else {
             header('HTTP/1.1 401 Unauthorized');
